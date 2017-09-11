c6d190bafb6f9200e021b99b8c5f34bc4c975975
tar <tar@localhost.localdomain>
2017-09-11 22:12:19 +0300

    first commit

---
+++ homework.js
@@ -0,0 +1,17 @@
+alert("zgush ape mtnum es mer mayla");
+
+const tzbex1 = 3;
+const tzbex2 = 7;
+if (tzbex1 + 4 = tzbex2){
+
+console.log("lav txu pay");
+
+}
+
+const gaxapar1 = 9;
+
+const gaxapar2 = 7;
+
+console.log (gaxapar1 + gaxapar2);
+
+console.log (tzbex1 + gaxapar1 / tzbex2);
